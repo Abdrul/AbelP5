d81c99be2c2687640bc9fa7e7954a65551652fa5
Abel <josephine.abel@outlook.fr>
2022-01-22 11:18:02 +0100

    optmisation de la partie js products

--- front/js/script.js
+++ front/js/script.js
@@ -1,51 +1,74 @@
 const items = document.getElementById('items')
 
-// instance de classe
 
-// let voiture = class {
-//     constructor(moteur) {
-//         this.moteur = moteur
-//     }
-// }
+// Part of Index 
 
-// let twingo = new voiture('ferrari')
+function getProducts () {
 
-
-function displayProducts() {
-
-    fetch(`http://localhost:3000/api/products`)
+    return fetch(`http://localhost:3000/api/products`)
 
     .then((reponse) => {
         return reponse.json()
     })
 
     .then((products) => {
-        // console.table(data);
-        
-        
-        for(let i = 0; i < products.length; i++) {
+        return products;
+    })
+
+    .catch(error => {
+        return [];
+    });
+
+}
+
+function renderProductsToHTML (products) {
+
+
+    for(let i = 0; i < products.length; i++) {
             
 
-            // J'ai pris les elements que je veux viser dans mon DOM (HTM INTO DOM) avec un .innerHTML
+        // J'ai pris les elements que je veux viser dans mon DOM (HTML INTO DOM) avec un .innerHTML
 
 
-            items.innerHTML +=
-            `<a href="./product.html?id=${products[i]._id}">
-                <article>
-                    <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
-                    <h3 class="productName">${products[i].name}</h3>
-                    <p class="productDescription">${products[i].description}</p>
-                </article>
-            </a>`
+        items.innerHTML +=
+        `<a href="./product.html?id=${products[i]._id}">
+            <article>
+                <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+                <h3 class="productName">${products[i].name}</h3>
+                <p class="productDescription">${products[i].description}</p>
+            </article>
+        </a>`
+        // const newDiv = document.createElement("div");
+        // newDiv.innerHTML = 
+        // `<a href="./product.html?id=${products[i]._id}">
+        //     <article>
+        //             <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+        //             <h3 class="productName">${products[i].name}</h3>
+        //             <p class="productDescription">${products[i].description}</p>
+        //     </article>
+        // </a>`
+        // items.appendChild(newDiv);
 
-                // console.log(data[i]);
-            }
-    
-        }
+            // console.log(data[i]);
+    }
 
-    )
+}
+
+async function displayProducts () {
+
+    const products = await getProducts();
+    if(products.length > 0) {
+        renderProductsToHTML(products);
+    } else {
+        items.innerHTML = "salut";
+    }
 
 }
 
 
 displayProducts()
+
+
+// document.createElement('tr)
+// const.innerHtml = <td>...</td>
+// const.appendChild()

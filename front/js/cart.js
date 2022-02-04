296ef256c1ba2738a96f16e8f735337378bde510
Abel <josephine.abel@outlook.fr>
2022-02-04 20:10:47 +0100

    fin de la fonction totaquantity et debut de la fonction totalcost

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,44 +6,168 @@
 
 // une fonction qui va calculer le prix total et une autre la quantité
 
+// supprimer du LS quand la quantite est a 0
+
+
 
 
 const cardItems = document.getElementById('cart__items');
 const totalPrice = document.getElementById('totalPrice');
+const totalQuantity = document.getElementById('totalQuantity');
 
-function getProducts () {
+// function getProducts () {
 
-    fetch(`http://localhost:3000/api/products`)
+//     fetch(`http://localhost:3000/api/products`)
 
-    .then(reponse => reponse.json())
+//     .then(reponse => reponse.json())
 
-    .then((products) => {
+//     .then((products) => {
 
-        const tab = JSON.parse(localStorage.getItem('cart'))
-        tab.forEach(product => {
-            let match = Object.values(products).find(element => element._id === product.id)
-            getCard(match, product)
-            // totalCost(tab,match)
-
-        })
-    })
+//         const tab = JSON.parse(localStorage.getItem('cart'))
+//         tab.forEach(product => {
+//             let match = Object.values(products).find(element => element._id === product.id)
+//             getCard(match, product)
+//             totalCost(tab,match)
+//             initQuantityListener()
+//         })
+//     })
     
+//     .catch(error => {
+//         [];
+//     });
+// }
+// getProducts()   
+
+function getProduct (id) {
+
+    return fetch(`http://localhost:3000/api/products/${id}`)
+
+    .then((reponse) => {
+        return reponse.json()
+    })
+
+    .then((product) => {
+        return product;
+    })
+
     .catch(error => {
-        [];
+        return [];
     });
 }
 
-getProducts()   
+
+async function displayProducts ()  {
+
+
+    const tab = JSON.parse(localStorage.getItem('cart'))
+    for(const product of tab) {
+        // console.log(product);
+        let match = await getProduct(product.id) 
+        getCard(match, product)
+        costProduct(match, product)
+    }
+    initQuantityListener ()
+    deleteCards()
+    countQuantityProducts (tab)
+} 
+
+displayProducts()
+
+
+
+function initQuantityListener () {
+
+    const inputQuantity = document.querySelectorAll('.itemQuantity')
+    // console.log(inputQuantity);
+    // console.log(Array.from(inputQuantity));
+    inputQuantity.forEach(input => {
+        input.addEventListener('change', (e) => {
+
+
+            // const inputArray = Array.from(inputQuantity)
+            // const total = inputArray.reduce((total, current) => {
+            //     return total += parseInt(current.value)
+            // }, 0)
+            // totalQuantity = total
+
+
+            let cartProduct = {
+                id : e.target.closest(".cart__item").dataset.id,
+                color : e.target.closest(".cart__item").dataset.color,
+                quantity : parseInt(e.target.closest(".itemQuantity").value)
+            }
+            // console.log(cartProduct);
+            const tab = JSON.parse(localStorage.getItem('cart')) || []
+            const index = tab.findIndex(element => element.id === cartProduct.id && element.color === cartProduct.color)
+            if(index !== -1) {
+                tab[index].quantity = cartProduct.quantity
+                localStorage.setItem('cart', JSON.stringify(tab))
+                countQuantityProducts (tab)
+
+                if(tab[index].quantity < 1) {
+
+                    tab.splice(index, 1)
+                    localStorage.setItem('cart', JSON.stringify(tab))
+                
+                } 
+
+            } 
+
+            
+        })
+
+    })
+}
+
+function countQuantityProducts (tab) {
+
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.quantity;
+    
+    totalQuantity.innerText = tab.reduce(reducer, 0);
+    
+
+}
+
+function costProduct (match, product) {
+
+    console.log();
+
+    totalPrice.innerText = match.price 
+
+}
+
+
+function deleteCards () {
+
+    const deleteItem = document.querySelectorAll('.deleteItem')
+    // console.log(deleteItem);
+    deleteItem.forEach(button => {
+        button.addEventListener("click", (e) => {
+            button.closest('.cart__item').remove()
+            // console.log(e.target.closest(".cart__item").dataset.id);
+
+            let cartProduct = {
+                id : e.target.closest(".cart__item").dataset.id,
+                color : e.target.closest(".cart__item").dataset.color
+
+            }
+            // console.log(cartProduct);
+
+            const tab = JSON.parse(localStorage.getItem('cart')) || []
+            console.log(tab);
+            const index = tab.findIndex(element => element.id === cartProduct.id && element.color === cartProduct.color)
+            if(index !== -1) {
+                tab.splice(index, 1)
+                localStorage.setItem('cart', JSON.stringify(tab))
+            }
+        })
+    })
+}
 
 
 function getCard(match, tab) {
     
-    
-    // const tab = JSON.parse(localStorage.getItem('cart'))
-
-
-    // localStorage.setItem('cart', JSON.stringify(tab))
-
     cardItems.innerHTML += 
         `<article class="cart__item" data-id="${tab.id}" data-color="${tab.color}">
         <div class="cart__item__img">
@@ -65,81 +189,10 @@ function getCard(match, tab) {
         </div>
         </div>
         </div>
-    </article>`
-
-    const inputQuantity = document.querySelectorAll('.itemQuantity')
-    // console.log(inputQuantity);
-    // console.log(Array.from(inputQuantity));
-    inputQuantity.forEach(input => {
-        input.addEventListener('change', () => {
-
-            // const tab = JSON.parse(localStorage.getItem('cart')) || []
-            // console.log(tab);
-
-            let tabInput = Array.from(inputQuantity)
-            // console.log(tabInput);
-            const total = tabInput.reduce((total, currentValue) => {
-                // console.log(currentValue);
-                // const value = currentValue.value === "" ? 0 : parseInt(currentValue.value)
-                // console.log(currentValue.value);
-                return total += parseInt(currentValue.value)
-                
-            },0)
-            
-            const totalQuantity = document.getElementById('totalQuantity');
-            
-            totalQuantity.textContent = total
-            
-            const totalPrice = document.getElementById('totalPrice');
-            
-            
-            // localStorage.setItem('cart', JSON.stringify(total))
-            
-        })
-
-    })
-
- 
-
-
-    const deleteItem = document.querySelectorAll('.deleteItem')
-    // console.log(deleteItem);
-    deleteItem.forEach(button => {
-        button.addEventListener("click", (e) => {
-            button.closest('.cart__item').remove()
-            // console.log(e.target.closest(".cart__item").dataset.id);
-
-            let cartProduct = {
-                id : e.target.closest(".cart__item").dataset.id,
-                color : e.target.closest(".cart__item").dataset.color
-
-            }
-            // console.log(cartProduct);
-
-            const tab = JSON.parse(localStorage.getItem('cart')) || []
-            console.log(tab);
-            const index = tab.findIndex(element => element.id === cartProduct.id && element.color === cartProduct.color)
-            if(index !== -1) {
-                tab.splice(index, 1)
-            }
-    
-            localStorage.setItem('cart', JSON.stringify(tab))
-
-
-        })
-    })
-
-    
+    </article>`    
 }
 
 
-// function totalCost (tab,match) {
-
-//     // let tabPrice = []
-//     totalPrice.textContent = match.price 
-//     console.log();
-    
-// }
 
 
 // const reducer = (accumulator, currentValue) => accumulator + currentValue;
